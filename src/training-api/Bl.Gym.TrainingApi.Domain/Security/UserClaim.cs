993f95367838124dab9783b3733dd34934b0922e
Guilherme Bley <guilhermecbley@gmail.com>
2024-05-24 22:29:41 -0300

    add security

---
+++ src/training-api/Bl.Gym.TrainingApi.Domain/Security/UserClaim.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace Bl.Gym.TrainingApi.Domain.Security;
+
+public static class UserClaim
+{
+    public const string DEFAULT_ROLE = "role";
+    public const string DEFAULT_USER_NAME = "name";
+    public const string DEFAULT_USER_ID = "nameidentifier";
+    public const string DEFAULT_USER_EMAIL = "emailaddress";
+    public static Claim SeeTraining => new(DEFAULT_ROLE, "SeeTraining");
+    public static Claim ManageTraining => new(DEFAULT_ROLE, "ManageTraining");
+
+    public static Claim CreateUserNameClaim(string userName)
+        => new(DEFAULT_USER_NAME, userName);
+    public static Claim CreateUserIdClaim(string userId)
+        => new(DEFAULT_USER_ID, userId);
+    public static Claim CreateUserEmailClaim(string email)
+        => new(DEFAULT_USER_EMAIL, email);
+}
---
+++ src/training-api/Bl.Gym.TrainingApi.Domain/Security/UserClaimDefaultGroup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+using System.Security.Claims;
+
+namespace Bl.Gym.TrainingApi.Domain.Security;
+
+public class UserClaimDefaultGroup
+    : IReadOnlyList<Claim>
+{
+    public static UserClaimDefaultGroup Student
+        => new(
+            new[]
+            {
+                UserClaim.SeeTraining
+            }
+        );
+
+    public static UserClaimDefaultGroup Teacher
+        => new(
+            new[]
+            {
+                UserClaim.ManageTraining,
+                UserClaim.SeeTraining
+            }
+        );
+
+    private readonly ImmutableArray<Claim> _claims;
+
+    private UserClaimDefaultGroup(IEnumerable<Claim> claims)
+        => _claims = claims.ToImmutableArray();
+
+    public Claim this[int index] => _claims[index];
+
+    public int Count => _claims.Length;
+
+    public IEnumerator<Claim> GetEnumerator()
+    {
+        return _claims.AsEnumerable().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _claims.AsEnumerable().GetEnumerator();
+    }
+}
