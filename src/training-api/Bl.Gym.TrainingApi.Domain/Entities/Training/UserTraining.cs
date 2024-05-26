b967aaac94e02f82de5b5e046ef73b6119dacdae
Guilherme Bley <guilhermecbley@gmail.com>
2024-05-26 09:06:49 -0300

    status to user training

--- src/training-api/Bl.Gym.TrainingApi.Domain/Entities/Training/UserTraining.cs
+++ src/training-api/Bl.Gym.TrainingApi.Domain/Entities/Training/UserTraining.cs
@@ -1,4 +1,6 @@
-﻿namespace Bl.Gym.TrainingApi.Domain.Entities.Training;
+﻿using Bl.Gym.TrainingApi.Domain.Enum;
+
+namespace Bl.Gym.TrainingApi.Domain.Entities.Training;
 
 /// <summary>
 /// A relationship between users and their exercises.
@@ -16,28 +18,57 @@ public class UserTraining
     /// The exercises from this training.
     /// </summary>
     public HashSet<Guid> ExerciseIds { get; private set; } = new();
+    public UserTrainingStatus Status { get; private set; }
+    public Guid ConcurrencyStamp { get; private set; }
     public DateTimeOffset CreatedAt { get; private set; }
 
     public override bool Equals(object? obj)
     {
         return obj is UserTraining training &&
                base.Equals(obj) &&
-               Id.Equals(training.Id) &&
                EntityId.Equals(training.EntityId) &&
+               Id.Equals(training.Id) &&
                StudentId.Equals(training.StudentId) &&
                GymId.Equals(training.GymId) &&
-               EqualityComparer<HashSet<Guid>>.Default.Equals(ExerciseIds, training.ExerciseIds);
+               EqualityComparer<HashSet<Guid>>.Default.Equals(ExerciseIds, training.ExerciseIds) &&
+               Status == training.Status &&
+               ConcurrencyStamp.Equals(training.ConcurrencyStamp) &&
+               CreatedAt.Equals(training.CreatedAt);
     }
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(base.GetHashCode(), Id, EntityId, StudentId, GymId, ExerciseIds);
+        HashCode hash = new HashCode();
+        hash.Add(base.GetHashCode());
+        hash.Add(EntityId);
+        hash.Add(Id);
+        hash.Add(StudentId);
+        hash.Add(GymId);
+        hash.Add(ExerciseIds);
+        hash.Add(Status);
+        hash.Add(ConcurrencyStamp);
+        hash.Add(CreatedAt);
+        return hash.ToHashCode();
     }
+
+    public static Result<UserTraining> CreateNew(
+        Guid id,
+        Guid studentId,
+        Guid gymId)
+        => Create(
+            id: id,
+            studentId: studentId,
+            gymId: gymId,
+            status: UserTrainingStatus.ToStart,
+            concurrencyStamp: Guid.NewGuid(),
+            createdAt: DateTimeOffset.UtcNow);
 
     public static Result<UserTraining> Create(
         Guid id,
         Guid studentId,
         Guid gymId,
+        UserTrainingStatus status,
+        Guid concurrencyStamp,
         DateTimeOffset createdAt)
     {
         ResultBuilder<UserTraining> builder = new();
@@ -49,6 +80,8 @@ public class UserTraining
                 CreatedAt = createdAt,
                 GymId = gymId,
                 StudentId = studentId,
+                ConcurrencyStamp = concurrencyStamp,
+                Status = status
             });
     }
 }
---
+++ src/training-api/Bl.Gym.TrainingApi.Domain/Enum/UserTrainingStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace Bl.Gym.TrainingApi.Domain.Enum;
+
+public enum UserTrainingStatus
+{
+    /// <summary>
+    /// Training to start
+    /// </summary>
+    ToStart = 0,
+    /// <summary>
+    /// Training in progress
+    /// </summary>
+    InProgress = 1,
+    /// <summary>
+    /// Training already finished
+    /// </summary>
+    Finished = 2,
+    /// <summary>
+    /// Training removed
+    /// </summary>
+    Removed
+}
