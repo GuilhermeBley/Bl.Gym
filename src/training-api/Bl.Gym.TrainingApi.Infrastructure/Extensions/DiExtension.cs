9d5a23a93281ad103fe77cb87ed2a477afbee039
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-05 21:50:06 -0300

    DiExtension

---
+++ src/training-api/Bl.Gym.TrainingApi.Infrastructure/Extensions/DiExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bl.Gym.TrainingApi.Infrastructure.Extensions;
+
+public static class DiExtension
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddDbContext<Application.Repositories.TrainingContext, Repositories.MySqlTrainingContext>();
+    }
+}
