b71db83303a397f5f52c132a9e0f386f75f87068
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-08 10:07:07 -0300

    adding controllers methods

--- src/training-api/Bl.Gym.TrainingApi.Api/Endpoints/TrainingEndpoint.cs
+++ src/training-api/Bl.Gym.TrainingApi.Api/Endpoints/TrainingEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bl.Gym.TrainingApi.Api.Endpoints;
 
@@ -13,6 +14,36 @@ public class TrainingEndpoint
 
             var result =
                 await mediator.Send(new Application.Commands.Training.GetAllTrainingsByCurrentUser.GetAllTrainingsByCurrentUserRequest());
+
+            if (result.Any())
+                return Results.Ok(result);
+
+            return Results.NoContent();
+
+        }).RequireAuthorization();
+
+        builder.MapPost("Training", async (
+            HttpContext context, 
+            IMediator mediator, 
+            [FromBody] Application.Commands.Training.CreateUserTraining.CreateTrainingToStudentRequest request) =>
+        {
+            var result =
+                await mediator.Send(request);
+
+            return Results.Ok(result);
+        }).RequireAuthorization();
+
+        builder.MapPatch("Training/{sectionId}/update-current-training-days", async (
+            HttpContext context, 
+            IMediator mediator, 
+            Guid sectionId,
+            [FromBody] Application.Commands.Training.UpdateCurrentDaysCountFromSection.UpdateCurrentDaysCountFromSectionRequest request) =>
+        {
+            if (sectionId != request.SectionId)
+                return Results.Unauthorized();
+
+            var result =
+                await mediator.Send(request);
 
             return Results.Ok(result);
         }).RequireAuthorization();
