e7292a3504a593d7441a70c4211527140298afec
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-11 20:25:38 -0300

    use endpoint

--- src/training-api/Bl.Gym.TrainingApi.Api/Program.cs
+++ src/training-api/Bl.Gym.TrainingApi.Api/Program.cs
@@ -30,6 +30,7 @@ app.UseMiddleware<Bl.Gym.TrainingApi.Api.Middleware.IdentityMiddleware>();
 
 #pragma warning disable ASP0014
 app.UseEndpoints(Bl.Gym.TrainingApi.Api.Endpoints.TrainingEndpoint.MapEndpoints);
+app.UseEndpoints(Bl.Gym.TrainingApi.Api.Endpoints.UserEndpoint.MapEndpoints);
 #pragma warning restore ASP0014
 
 app.Run();
