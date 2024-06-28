25430879372cbfe576b8b4722179c7fa950810e3
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-27 23:38:30 -0300

    adding user context

--- src/ui/Bl-gym-mobile/src/contexts/UserContext.tsx
+++ src/ui/Bl-gym-mobile/src/contexts/UserContext.tsx
@@ -1,8 +1,29 @@
 import { createContext } from "react";
 
-export const UserContext = createContext({});
+interface UserContextProps{
+    id: string;
+    name: string;
+    email: string;
+    roles: string[];
+    authorized: boolean;
+    isInRole(roles: string | string[]) : boolean;
+    setUserByJwtToken(jwtToken: string) : void;
+}
 
-export default function UserContextProvider({children} : any, user: any){
+const unauthorizedUser: UserContextProps = {
+    id: "",
+    name: "",
+    email: "",
+    roles: [],
+    authorized: false,
+    isInRole: () => false,
+    setUserByJwtToken: () => { }
+};
+
+export const UserContext = createContext<UserContextProps>(unauthorizedUser);
+
+export default function UserContextProvider({children} : any, user: UserContextProps){
+
     return (
         <UserContext.Provider value={user}>
             {children}
