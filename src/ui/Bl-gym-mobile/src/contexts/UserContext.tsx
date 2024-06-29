609f99902f94a11f83d9e58587de8e47472d3241
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-29 18:29:46 -0300

    checking user

--- src/ui/Bl-gym-mobile/src/contexts/UserContext.tsx
+++ src/ui/Bl-gym-mobile/src/contexts/UserContext.tsx
@@ -1,24 +1,62 @@
 import { createContext } from "react";
+import jwtDecoder from 'jwt-decode';
 
-interface UserContextProps{
+class UserContextProps{
     id: string;
     name: string;
     email: string;
     roles: string[];
     authorized: boolean;
-    isInRole(roles: string | string[]) : boolean;
-    setUserByJwtToken(jwtToken: string) : void;
+
+    constructor(id: string, name: string, email: string, roles: string[], authorized: boolean) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.roles = roles;
+        this.authorized = authorized;
+    }
+
+    isInRole(roles: string | string[]): boolean {
+        if (typeof roles === 'string') {
+            return this.roles.includes(roles);
+        }
+        return roles.some(role => this.roles.includes(role));
+    }
+
+    setUserByJwtToken(jwtToken: string): void {
+        try{
+            const decoded: any = jwtDecoder.jwtDecode(jwtToken);
+
+            if (!decoded ||
+                parseInt(decoded.id) < 1)
+                this.setAsUnauthorized();
+                return;
+    
+            // Update the user properties based on the decoded token
+            this.id = decoded.id;
+            this.name = decoded.name;
+            this.email = decoded.email;
+            this.roles = decoded.roles;
+            this.authorized = decoded.authorized;
+        }
+        catch{
+            console.error("Failed to parse login.")
+        }
+    }
+
+    setAsUnauthorized()
+    {
+        this.id = "";
+        this.name = "";
+        this.email = "";
+        this.roles = [];
+        this.authorized = false;
+    }
 }
 
-const unauthorizedUser: UserContextProps = {
-    id: "",
-    name: "",
-    email: "",
-    roles: [],
-    authorized: false,
-    isInRole: () => false,
-    setUserByJwtToken: () => { }
-};
+const unauthorizedUser = new UserContextProps(
+    "", "", "", [], false
+);
 
 export const UserContext = createContext<UserContextProps>(unauthorizedUser);
 
