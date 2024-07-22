10bcff0813291ac9e3eb517797b8173cb776c233
Guilherme Bley <guilhermecbley@gmail.com>
2024-07-21 22:11:42 -0300

    adding SendMessageAsync

--- src/training-api/Bl.Gym.TrainingApi.Application/Commands/Identity/RequestToChangePassword/RequestToChangePasswordHandler.cs
+++ src/training-api/Bl.Gym.TrainingApi.Application/Commands/Identity/RequestToChangePassword/RequestToChangePasswordHandler.cs
@@ -1,5 +1,50 @@
+
+using Bl.Gym.EventBus;
+using Bl.Gym.TrainingApi.Application.Repositories;
+
 namespace Bl.Gym.TrainingApi.Application.Commands.Identity.RequestToChangePassword;
 
-public record RequestToChangePasswordRequest(
-    string Email
-) : IRequest<RequestToChangePasswordResponse>;
+public class RequestToChangePasswordHandler
+    : IRequestHandler<RequestToChangePasswordRequest, RequestToChangePasswordResponse>
+{
+    private readonly TrainingContext _context;
+    private readonly ILogger<RequestToChangePasswordHandler> _logger;
+    private readonly IEventBus _eventBus;
+
+    public RequestToChangePasswordHandler(
+        TrainingContext context,
+        ILogger<RequestToChangePasswordHandler> logger,
+        IEventBus eventBus)
+    {
+        _context = context;
+        _logger = logger;
+        _eventBus = eventBus;
+    }
+    
+    public async Task<RequestToChangePasswordResponse> Handle(
+        RequestToChangePasswordRequest request,
+        CancellationToken cancellationToken)
+    {
+        var user = await _context
+            .Users
+            .AsNoTracking()
+            .Where(e => e.Email == request.Email)
+            .Select(e => new { e.Id })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (user != null)
+        {
+            //
+            // Enqueue message to send to email
+            //
+            await _eventBus.SendMessageAsync(
+                new EventBus.Events.UserRequestingToChangePasswordEvent()
+                {
+                    UserId = user!.Id
+                });
+            return new(true);
+        }
+        
+        return new(false);
+    }
+}
