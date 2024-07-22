6f83d2ab3522243848707796ca7ccad23c93a61d
Guilherme Bley <guilhermecbley@gmail.com>
2024-07-21 22:53:57 -0300

    adding azure event bus to DI

---
+++ src/BuildingBlocks/EventBus/Bl.Gym.EventBus.Infrastructure/AzureEventBus.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Bl.Gym.EventBus.Infrastructure;
+
+public class AzureEventBus
+    : IEventBus, IAsyncDisposable, IDisposable
+{
+    private readonly ServiceBusClient _client;
+
+    public AzureEventBus(string connectionsString)
+    {
+        _client = new ServiceBusClient(connectionsString);
+    }
+
+    public void Dispose()
+    {
+        _client.DisposeAsync().GetAwaiter().GetResult();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return _client.DisposeAsync();
+    }
+
+    async Task IEventBus.SendMessageAsync<TEvent>(
+        TEvent @event, 
+        CancellationToken cancellationToken)
+    {
+        var jsonMessage = System.Text.Json.JsonSerializer.Serialize(@event);
+
+        var message = new ServiceBusMessage(jsonMessage)
+        {
+            MessageId = Guid.NewGuid().ToString(),
+        };
+
+        await using var sender = _client.CreateSender(
+            GetQueueOrTopicNameByEvent(@event));
+
+        await sender.SendMessageAsync(message, cancellationToken);
+    }
+
+    private static string GetQueueOrTopicNameByEvent<TEvent>(
+        TEvent @event)
+        => nameof(TEvent);
+}
---
+++ src/training-api/Bl.Gym.TrainingApi.Infrastructure/Options/AzureEventbusOption.cs
@@ -0,0 +1,7 @@
+﻿namespace Bl.Gym.TrainingApi.Infrastructure.Options;
+
+public class AzureEventbusOption
+{
+    public const string SECTION = nameof(AzureEventbusOption);
+    public string ConnectionsString { get; set; } = string.Empty;
+}
