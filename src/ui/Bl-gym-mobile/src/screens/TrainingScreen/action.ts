6013a48314e8bfb6b6bf29929fd883c83261b1e0
Guilherme Bley <guilhermecbley@gmail.com>
2024-08-03 09:51:15 -0300

    adding trainig screen

--- src/ui/Bl-gym-mobile/src/screens/TrainingScreen/action.ts
+++ src/ui/Bl-gym-mobile/src/screens/TrainingScreen/action.ts
@@ -1,5 +1,13 @@
-const getTrainingInfoById = (
-    trainingId: string
+import { CancelToken } from "axios";
+import axios  from "../../api/GymApi"
+
+export const getTrainingInfoById = (
+    trainingId: string,
+    cancellationToken: CancelToken
 ) => {
-    
+    return axios
+        .get("", { cancelToken: cancellationToken })
+        .then(response => {
+            return []
+        })
 }
--- src/ui/Bl-gym-mobile/src/screens/TrainingScreen/index.tsx
+++ src/ui/Bl-gym-mobile/src/screens/TrainingScreen/index.tsx
@@ -1,11 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { TrainingContext } from "../../contexts/TrainingContext";
 import { UserContext } from "../../contexts/UserContext";
+import axios from "axios";
+import { getTrainingInfoById } from "./action";
 
 const TrainingScreen = () => {
+    const [trainingInfo, setTrainingInfo] = useState({
+        data: []
+    });
+
     const trainingContext = useContext(TrainingContext)
     const userContext = useContext(UserContext)
+
+    useEffect(() => {
+        const source = axios.CancelToken.source();
+
+        const fetchTraning = async () => {
+
+            if (trainingContext.training === undefined)
+                return;
+
+            let trainings = await getTrainingInfoById(
+                trainingContext.training.trainingId,
+                source.token);
+            
+            setTrainingInfo(previous => ({
+                ...previous,
+                data: trainings
+            }))
+            
+        }
+
+        fetchTraning();
+
+        return () => {
+            source.cancel();
+        }
+
+    }, [])
 
     return trainingContext.training === undefined ?
         (
