700874cbaba17c6ce363db51439caa364fab3a33
Guilherme Bley <guilhermecbley@gmail.com>
2024-08-11 23:06:03 -0300

    Gym creation form

--- src/ui/Bl-gym-mobile/src/components/gym/CreateGymModalWithManageAnyGymRole/index.tsx
+++ src/ui/Bl-gym-mobile/src/components/gym/CreateGymModalWithManageAnyGymRole/index.tsx
@@ -1,12 +1,52 @@
-import { Modal, View, Text, Button } from "react-native";
+import { Modal, View, Text, Button, ActivityIndicator, TextInput } from "react-native";
 import { styles } from "./styles";
 import React from "react";
+import { Formik, FormikProps } from "formik";
 
 type ChildComponentProps = {
     modalVisible: boolean,
     setModalVisible: React.Dispatch<React.SetStateAction<boolean>>
 };
-  
+
+interface GymCreateModel {
+    gymName: string,
+    description: string | undefined
+}
+
+interface StyledInputProps {
+  formikKey: string,
+  formikProps: FormikProps<any>;
+  label: string;
+  [key: string]: any;
+}
+
+const StyledInput: React.FC<StyledInputProps> = ({ formikKey, formikProps, label, ...rest }) => {
+    const inputStyles = styles.input;
+
+    const error = formikProps.touched[formikKey] && formikProps.errors[formikKey];
+    const errorMessage = typeof error === 'string' ? error : '';
+
+    if (errorMessage) {
+        inputStyles.borderColor = "red"
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+            <Text>{label}</Text>
+            <TextInput
+                style={inputStyles}
+                value={formikProps.values[formikKey]}
+                onChangeText={formikProps.handleChange(formikKey)}
+                onBlur={formikProps.handleBlur(formikKey)}
+                {...rest}
+            >
+
+            </TextInput>
+            {errorMessage ? <Text style={{ color: 'red' }}>{errorMessage}</Text> : null}
+        </View>
+    );
+}
+
 const CreateGymModalWithManageAnyGymRole = ({ modalVisible, setModalVisible }: ChildComponentProps) => {
     return (
         <Modal
@@ -16,9 +56,53 @@ const CreateGymModalWithManageAnyGymRole = ({ modalVisible, setModalVisible }: C
             onRequestClose={() => {
                 setModalVisible(false);
             }}>
+
             <View style={styles.modalView}>
-                <Text style={styles.modalText}>Hello, I'm a Modal!</Text>
-                <Button title="Close Modal" onPress={() => setModalVisible(false)} />
+
+                <Formik
+                    initialValues={{
+                        description: undefined,
+                        gymName: ""
+                    } as GymCreateModel}
+                    onSubmit={(values, actions) => {
+                        console.debug("handling gym creation...")
+
+                        return handleSubmit(actions, values)
+                            .finally(() => actions.setSubmitting(false));
+                    }
+                    }
+                    validationSchema={validationSchema}
+                >
+
+                    {formikProps => {
+                        return (
+                            <React.Fragment>
+                                <StyledInput
+                                    formikKey={"gymName"}
+                                    formikProps={formikProps}
+                                    label={"Nome da academia"}
+                                    autoFocus />
+
+                                <StyledInput
+                                    formikKey={"description"}
+                                    formikProps={formikProps}
+                                    label={"Descrição"} />
+
+                                <View style={styles.buttonContainer}>
+                                    {formikProps.isSubmitting ?
+                                        <ActivityIndicator /> :
+                                        <Button onPress={() => formikProps.handleSubmit()} title="Criar acedemia" />}
+                                </View>
+
+                                <View>
+                                    <Text style={{ color: 'red' }}>{formikProps.errors[responseErrorsKey as keyof GymCreateModel]}</Text>
+                                </View>
+                            </React.Fragment>
+                        );
+                    }}
+                </Formik>
+
+                <Button title="Fechar" onPress={() => setModalVisible(false)} />
             </View>
         </Modal>
     );
