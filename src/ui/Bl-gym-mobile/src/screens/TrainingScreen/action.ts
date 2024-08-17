41a8286023c711407187330e17e31364d681bab9
Guilherme Bley <guilhermecbley@gmail.com>
2024-08-16 23:19:53 -0300

    getting training details

--- src/ui/Bl-gym-mobile/src/screens/TrainingScreen/action.ts
+++ src/ui/Bl-gym-mobile/src/screens/TrainingScreen/action.ts
@@ -2,14 +2,45 @@ import { AxiosError, CancelToken } from "axios";
 import axios from "../../api/GymApi"
 import TryGetResultFromResponse from "../../api/ResponseReader";
 
+interface GetTrainingInfoByIdResponse{
+    Section: GetTrainingInfoByIdResponseSection,
+    Status: string,
+    CreatedAt: Date
+}
+
+interface GetTrainingInfoByIdResponseSection{
+    SectionId: string,
+    MuscularGroup: string,
+    TargetDaysCount: number,
+    CurrentDaysCount: number,
+    ConcurrencyStamp: string,
+    Sets: GetTrainingInfoByIdResponseExerciseSet[],
+    CreatedAt: Date,
+}
+
+interface GetTrainingInfoByIdResponseExerciseSet{
+    Set: string,
+    Title: string,
+    Description: string,
+    CreatedAt: Date,
+}
+
 export const getTrainingInfoById = (
-    trainingId: string,
+    sheetId: string,
+    userId: string,
     cancellationToken: CancelToken
 ) => {
     return axios
-        .get("", { cancelToken: cancellationToken })
+        .get<GetTrainingInfoByIdResponse>(
+            "Training/details/{userId}/sheet/{sheetId}"
+                .replace("{userId}", userId)
+                .replace("{sheetId}", sheetId),
+            { cancelToken: cancellationToken })
         .then(response => {
-            return []
+            return TryGetResultFromResponse(response)
+        })
+        .catch((error: AxiosError<GetTrainingInfoByIdResponse>) => {
+            return TryGetResultFromResponse(error.response)
         })
 }
 
