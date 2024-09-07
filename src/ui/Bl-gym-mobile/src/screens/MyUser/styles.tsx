b78e87e779dd4ec2053ad7ce3abbb47f665a0e8e
Guilherme Bley <guilhermecbley@gmail.com>
2024-09-07 16:58:37 -0300

    user info page

--- src/ui/Bl-gym-mobile/src/screens/MyUser/styles.tsx
+++ src/ui/Bl-gym-mobile/src/screens/MyUser/styles.tsx
@@ -1,12 +1,54 @@
 import { StyleSheet } from "react-native";
+import colors from "../../styles/colors";
 
 const styles = StyleSheet.create({
   container: {
     marginEnd: "3%",
     marginStart: "3%",
     height: "100%",
-    justifyContent: 'center',
     marginTop: "auto",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  userInfo: {
+    marginBottom: 30,
+    width: '100%',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  value: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  logoutButton: {
+    backgroundColor: colors.primary,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+  },
+  logoutText: {
+    color: '#FFF',
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: '600',
+  },
+  separatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: 'black',
+  },
+  separatorText: {
+    marginHorizontal: 10,
   },
 });
   
