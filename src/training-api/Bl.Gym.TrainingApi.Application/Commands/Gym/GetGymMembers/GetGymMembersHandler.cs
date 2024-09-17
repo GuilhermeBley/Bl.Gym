427e86c18745d88271a66a64ef04f321d43641ce
Guilherme Bley <guilhermecbley@gmail.com>
2024-09-16 23:40:09 -0300

    GetGymMembersHandler

---
+++ src/training-api/Bl.Gym.TrainingApi.Application/Commands/Gym/GetGymMembers/GetGymMembersHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Bl.Gym.TrainingApi.Application.Commands.Gym.GetGymMembers;
+
+public class GetGymMembersHandler
+    : IRequestHandler<GetGymMembersRequest, GetGymMembersResponse>
+{
+    public Task<GetGymMembersResponse> Handle(GetGymMembersRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/training-api/Bl.Gym.TrainingApi.Application/Commands/Gym/GetGymMembers/GetGymMembersResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Bl.Gym.TrainingApi.Application.Commands.Gym.GetGymMembers;
+
+public record GetGymMembersResponse(
+    Guid UserId,
+    string Email,
+    string Name,
+    string LastName);
