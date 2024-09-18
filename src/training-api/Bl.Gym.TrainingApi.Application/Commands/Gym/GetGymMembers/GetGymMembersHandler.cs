fc5b80fe7005ea6f42e3329329e667bedf1fb1aa
Guilherme Bley <guilhermecbley@gmail.com>
2024-09-17 23:53:05 -0300

    GetGymMembersHandler

--- src/training-api/Bl.Gym.TrainingApi.Application/Commands/Gym/GetGymMembers/GetGymMembersHandler.cs
+++ src/training-api/Bl.Gym.TrainingApi.Application/Commands/Gym/GetGymMembers/GetGymMembersHandler.cs
@@ -1,11 +1,33 @@
 ﻿
+using Bl.Gym.TrainingApi.Application.Providers;
+using Bl.Gym.TrainingApi.Application.Repositories;
+using Bl.Gym.TrainingApi.Application.Services;
+
 namespace Bl.Gym.TrainingApi.Application.Commands.Gym.GetGymMembers;
 
 public class GetGymMembersHandler
     : IRequestHandler<GetGymMembersRequest, GetGymMembersResponse>
 {
-    public Task<GetGymMembersResponse> Handle(GetGymMembersRequest request, CancellationToken cancellationToken)
+    private readonly TrainingContext _trainingContext;
+    private readonly GymRoleCheckerService _gymRoleCheckerService;
+    private readonly IIdentityProvider _identityProvider;
+
+    public async Task<GetGymMembersResponse> Handle(
+        GetGymMembersRequest request, 
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+
+        var currentUser = await _identityProvider.GetCurrentAsync(cancellationToken);
+
+        //
+        // Only 'instructor'(or above) can access the list of students
+        //
+        await _gymRoleCheckerService.ThrowIfUserDoesNotContainRoleInGymAsync(
+            currentUser.RequiredUserId(),
+            request.GymId,
+            role: Domain.Security.UserClaim.ManageTraining.Value,
+            cancellationToken);
+
+
     }
 }
--- src/training-api/Bl.Gym.TrainingApi.Application/Commands/Gym/GetGymMembers/GetGymMembersResponse.cs
+++ src/training-api/Bl.Gym.TrainingApi.Application/Commands/Gym/GetGymMembers/GetGymMembersResponse.cs
@@ -1,6 +1,9 @@
 ﻿namespace Bl.Gym.TrainingApi.Application.Commands.Gym.GetGymMembers;
 
 public record GetGymMembersResponse(
+    GetGymMembersItemResponse[] Students);
+
+public record GetGymMembersItemResponse(
     Guid UserId,
     string Email,
     string Name,
