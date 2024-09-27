2ee29f453142c2455c896c3622fc8641fcf3b823
Guilherme Bley <guilhermecbley@gmail.com>
2024-09-26 22:43:20 -0300

    changing default buttons

--- src/ui/Bl-gym-mobile/src/styles/colors.tsx
+++ src/ui/Bl-gym-mobile/src/styles/colors.tsx
@@ -1,5 +1,5 @@
 export default {
-    primary: '#3498db',
+    primary: '#0d6efd',
     secondary: '#2ecc71',
     white: '#ffffff',
     black: '#000000'
--- src/ui/Bl-gym-mobile/src/styles/commonStyles.tsx
+++ src/ui/Bl-gym-mobile/src/styles/commonStyles.tsx
@@ -9,11 +9,22 @@ const styles = StyleSheet.create({
       textAlign: 'center'  
   },
   PrimaryButton: {
-    color: colors.primary,
-    width: "100%"
+    backgroundColor: colors.primary,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3, // Adds shadow on Android
+    shadowColor: '#000', // Adds shadow on iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   PrimaryButtonText: {
-    
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   }
 });
 
