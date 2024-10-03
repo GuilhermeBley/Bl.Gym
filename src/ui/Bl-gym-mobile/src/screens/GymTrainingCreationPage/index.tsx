50b93eeb7c54713549c26cf1794b2353e9175ada
Guilherme Bley <guilhermecbley@gmail.com>
2024-10-02 22:34:43 -0300

    adding styled select

--- src/ui/Bl-gym-mobile/src/screens/GymTrainingCreationPage/index.tsx
+++ src/ui/Bl-gym-mobile/src/screens/GymTrainingCreationPage/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { SafeAreaView, TextInput, View, Text, ActivityIndicator, Button } from "react-native";
 import * as yup from 'yup';
 import { styles } from "./styles";
+import { Picker } from '@react-native-picker/picker';
 
 interface TrainingCreationModel {
   gymId: string,
@@ -34,6 +35,44 @@ const GymTrainingCreationPage = () => {
     trainingStudentId: "",
     studentName: "",
     trainingData: undefined
+  };
+
+  interface StyledSelectProps {
+    formikKey: string;
+    formikProps: any;
+    label: string;
+    options: { label: string; value: string }[];
+    [key: string]: any;
+  }
+  
+  const StyledSelect: React.FC<StyledSelectProps> = ({ formikKey, formikProps, label, options, ...rest }) => {
+    const inputStyles = styles.input;
+  
+    const error = formikProps.touched[formikKey] && formikProps.errors[formikKey];
+    const errorMessage = typeof error === 'string' ? error : '';
+  
+    if (errorMessage) {
+      inputStyles.borderColor = 'red';
+    }
+  
+    return (
+      <View style={styles.inputContainer}>
+        <Text>{label}</Text>
+        <View style={[inputStyles, { paddingHorizontal: 5 }]}>
+          <Picker
+            selectedValue={formikProps.values[formikKey]}
+            onValueChange={formikProps.handleChange(formikKey)}
+            onBlur={formikProps.handleBlur(formikKey)}
+            {...rest}
+          >
+            {options.map((option) => (
+              <Picker.Item key={option.value} label={option.label} value={option.value} />
+            ))}
+          </Picker>
+        </View>
+        {errorMessage ? <Text style={{ color: 'red' }}>{errorMessage}</Text> : null}
+      </View>
+    );
   };
 
   const StyledInput: React.FC<StyledInputProps> = ({ formikKey, formikProps, label, ...rest }) => {
