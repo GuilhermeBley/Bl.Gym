d60b4f07cea03c3a95b115bb742a833091411779
Guilherme Bley <guilhermecbley@gmail.com>
2024-11-01 23:44:46 -0300

    feat(UserTrainingSheetModel) - adding model

---
+++ src/training-api/Bl.Gym.TrainingApi.Application/Model/Training/TrainingUserPeriodModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Bl.Gym.TrainingApi.Application.Model.Training;
+
+/// <summary>
+/// This model represents an relation between User and Training Section.
+/// </summary>
+public class TrainingUserPeriodModel
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid SectionId { get; set; }
+
+    public DateTime? StartedAt { get; set; }
+    public DateTime? EndedAt { get; set; }
+    public string? Observation { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
