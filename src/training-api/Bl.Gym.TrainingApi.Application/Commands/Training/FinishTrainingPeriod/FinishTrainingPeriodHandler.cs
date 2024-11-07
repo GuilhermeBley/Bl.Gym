363e0d0d7c595196d2dff37070258d988a17765a
Guilherme Bley <guilhermecbley@gmail.com>
2024-11-06 23:35:53 -0300

    feat(FinishTrainingPeriodHandler) - creating handler

---
+++ src/training-api/Bl.Gym.TrainingApi.Application/Commands/Training/FinishTrainingPeriod/FinishTrainingPeriodHandler.cs
@@ -0,0 +1,92 @@
+﻿
+using Bl.Gym.TrainingApi.Application.Providers;
+using Bl.Gym.TrainingApi.Application.Repositories;
+using Bl.Gym.TrainingApi.Application.Services;
+
+namespace Bl.Gym.TrainingApi.Application.Commands.Training.FinishTrainingPeriod;
+
+public class FinishTrainingPeriodHandler
+    : IRequestHandler<FinishTrainingPeriodRequest, FinishTrainingPeriodResponse>
+{
+    private readonly IIdentityProvider _identityProvider;
+    private readonly TrainingContext _context;
+    private readonly GymRoleCheckerService _gymChecker;
+    private readonly ILogger<FinishTrainingPeriodHandler> _logger;
+
+    public FinishTrainingPeriodHandler(
+        IIdentityProvider identityProvider,
+        TrainingContext context,
+        GymRoleCheckerService gymChecker,
+        ILogger<FinishTrainingPeriodHandler> logger)
+    {
+        _identityProvider = identityProvider;
+        _context = context;
+        _gymChecker = gymChecker;
+        _logger = logger;
+    }
+
+    public async Task<FinishTrainingPeriodResponse> Handle(
+        FinishTrainingPeriodRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var user = await _identityProvider.GetCurrentAsync(cancellationToken);
+
+        user.ThrowIfDoesntContainRole(Domain.Security.UserClaim.SeeTraining);
+
+        var periodToUpdate = await _context
+            .TrainingsPeriod
+            .Where(e => e.Id == request.PeriodId)
+            .FirstOrDefaultAsync(cancellationToken)
+            ?? throw CoreException.CreateByCode(CoreExceptionCode.NotFound);
+
+        var periodEntity = null; // TODO: create entity and set as finished
+
+        await _gymChecker.ThrowIfUserIsntInTheSectionAsync(user.RequiredUserId(), periodToUpdate.SectionId, cancellationToken);
+
+        var sheetFound = await
+            (from sheet in _context.UserTrainingSheets.AsNoTracking()
+             join section in _context.TrainingSections.AsNoTracking()
+                 on sheet.Id equals section.UserTrainingSheetId
+             select sheet).FirstOrDefaultAsync(cancellationToken)
+             ?? throw CoreException.CreateByCode(CoreExceptionCode.NotFound);
+
+        if (sheetFound.Status != Domain.Enum.UserTrainingStatus.InProgress)
+        {
+            _logger.LogInformation("Sheet {0} was not started.", sheetFound.Id);
+            throw CoreException.CreateByCode(CoreExceptionCode.ThisUserSheetIsntStarted);
+        }
+
+        var sectionFound = await _context
+            .TrainingSections
+            .AsNoTracking()
+            .Where(e => e.Id == periodToUpdate.SectionId)
+            .FirstOrDefaultAsync(cancellationToken)
+             ?? throw CoreException.CreateByCode(CoreExceptionCode.NotFound);
+
+        var entityToUpdate = sectionFound.MapToEntity();
+
+        entityToUpdate.UpdateCurrentDaysCount(entityToUpdate.CurrentDaysCount)
+            .EnsureSuccess();
+
+        await using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+
+        var affetedRows = await _context
+            .TrainingSections
+            .Where(e => e.Id == periodToUpdate.SectionId)
+            .Where(e => e.ConcurrencyStamp == entityToUpdate.ConcurrencyStamp)
+            .ExecuteUpdateAsync(setter => setter.SetProperty(p => p.CurrentDaysCount, entityToUpdate.CurrentDaysCount));
+
+        if (affetedRows != 0)
+            throw CoreException.CreateByCode(CoreExceptionCode.ThisEntityWasAlreadyUpdateByAnotherSource);
+
+        //
+        // TODO: Update field according to entity
+        //
+        periodToUpdate.UpdatedAt = DateTime.UtcNow;
+        periodToUpdate.EndedAt = DateTime.UtcNow;
+
+        await transaction.CommitAsync();
+
+        return new();
+    }
+}
