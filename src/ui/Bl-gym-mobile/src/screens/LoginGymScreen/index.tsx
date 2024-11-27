7982e11714342d85274cc6797ffb1755d5a66dd5
Guilherme Bley <guilhermecbley@gmail.com>
2024-11-26 23:36:09 -0300

    feat(LoginGymScreen) - adding initial loading #17

--- src/ui/Bl-gym-mobile/src/screens/LoginGymScreen/index.tsx
+++ src/ui/Bl-gym-mobile/src/screens/LoginGymScreen/index.tsx
@@ -1,17 +1,61 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import GymCardComponent, { GymCardInfo } from "../../components/GymCardComponent";
+import { getGyms } from "./action";
+import { UserContext } from "../../contexts/UserContext";
+import axios from 'axios';
 
 const LoginGymScreen = () => {
 
     const [pageData, setPageData] = useState<{
         isLoadingInitialData: boolean
+        initialErrors: string[]
         Gyms: GymCardInfo[]
     }>({
         isLoadingInitialData: true,
+        initialErrors: [],
         Gyms: []
     });
+
+    const userCtx = useContext(UserContext);
+
+    useEffect(() => {
+
+        var src = axios.CancelToken.source();
+
+        const handleInitialPageLoading = async () => {
+
+            try
+            {
+                var gymsResult = await getGyms(userCtx.user.id);
+    
+                if (!gymsResult.Success)
+                {
+                    setPageData(prev => ({
+                        ...prev,
+                        initialErrors: gymsResult.Errors
+                    }));
+                }
+
+                setPageData(prev => ({
+                    ...prev,
+                    Gyms: gymsResult.Data.gyms
+                }));
+            }
+            finally
+            {
+                setPageData(prev => ({
+                    ...prev,
+                    isLoadingInitialData: false
+                }));
+            }
+        };
+
+        handleInitialPageLoading();
+
+        return () => { src.cancel(); }
+    }, [])
 
     return (
         <SafeAreaView>
