ce751d715486c80283526aa9738965493bd2accd
Guilherme Bley <guilhermecbley@gmail.com>
2024-12-15 22:37:35 -0300

    feat(Api) creating InvitationTokenGenerator #22

---
+++ src/training-api/Bl.Gym.TrainingApi.Api/Services/InvitationTokenGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Bl.Gym.TrainingApi.Api.Options;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace Bl.Gym.TrainingApi.Api.Services;
+
+public class InvitationTokenGenerator
+{
+    private readonly IOptions<Options.JwtOptions> _options;
+
+    public InvitationTokenGenerator(IOptions<JwtOptions> options)
+    {
+        _options = options;
+    }
+
+    public string Generate(
+        Claim[] claims, 
+        DateTime expiresIn)
+    {
+        throw new NotImplementedException();
+    }
+}
