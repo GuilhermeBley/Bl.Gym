8183175492c20e311b89271bbb8a1f2e9bc8e1d1
Guilherme Bley <guilhermecbley@gmail.com>
2025-01-02 23:59:38 -0300

    moving comments

--- src/ui/Bl-gym-mobile/App.tsx
+++ src/ui/Bl-gym-mobile/App.tsx
@@ -6,10 +6,11 @@ import TrainingContextProvider from './src/contexts/TrainingContext';
 import { ErrorProvider } from './src/contexts/ErrorContext';
 import FloatErrorComponent from './src/components/FloatErrorComponent';
 
+/*Use the FloatErrorComponent to show the error in the top.*/
 export default function App() {
   return (
     <ErrorProvider>
-      <FloatErrorComponent /> {/*Use this to show the error in the top.*/}
+      <FloatErrorComponent /> 
       <UserContextProvider>
         <TrainingContextProvider>
           <Router></Router>
