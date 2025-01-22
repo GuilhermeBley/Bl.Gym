98be1ef1eebfc5b8aa7d4bb47a63524b38de557e
Guilherme Bley <guilhermecbley@gmail.com>
2025-01-21 22:08:30 -0300

    feat(accordingcomponent)

---
+++ src/ui/Bl-gym-mobile/src/components/AccordionComponent/styles.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+    section: {
+        marginBottom: 10,
+    },
+    header: {
+        backgroundColor: '#007BFF',
+        padding: 15,
+        borderRadius: 5,
+    },
+    headerText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    body: {
+        overflow: 'hidden',
+        backgroundColor: '#F5F5F5',
+        paddingHorizontal: 10,
+    },
+});
